325d6326231732eae139b9b536c0e571cedbbfb4
babalolajnr <babalolajnr@gmail.com>
2021-02-16 08:33:39 +0100

    added get single product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -6,7 +6,7 @@ export class ProductsService {
   products: Product[] = [];
 
   insertProduct(title: string, description: string, price: number) {
-    const prodID = new Date().toString();
+    const prodID = Math.random().toString();
     const newProduct = new Product(prodID, title, description, price);
     this.products.push(newProduct);
     return prodID;
@@ -14,5 +14,13 @@ export class ProductsService {
 
   getProducts() {
     return [...this.products];
+  }
+
+  getSingleProduct(productID: string) {
+    const product = this.products.find((prod) => prod.id === productID);
+    if (!product) {
+      throw new NotFoundException('Could not find product');
+    }
+    return { ...product };
   }
 }
