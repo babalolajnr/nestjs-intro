0447af23967ce27b2a802f54ba81e8866d8eea6b
babalolajnr <babalolajnr@gmail.com>
2021-02-16 12:32:00 +0100

    added update product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -17,10 +17,36 @@ export class ProductsService {
   }
 
   getSingleProduct(productID: string) {
-    const product = this.products.find((prod) => prod.id === productID);
+    const product = this.findProduct(productID)[0];
+    return { ...product };
+  }
+
+  updateProduct(
+    productId: string,
+    title: string,
+    description: string,
+    price: number,
+  ) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (description) {
+      updatedProduct.description = description;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = { ...product };
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
     if (!product) {
       throw new NotFoundException('Could not find product');
     }
-    return { ...product };
+    return [product, productIndex];
   }
 }
